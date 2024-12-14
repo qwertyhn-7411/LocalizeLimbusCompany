6fb82d32fc8790dabba0f9123915b0ed31cba3b2
qwertyhn-7411 <3795866459@QQ.com>
2024-12-15 01:18:16 +0800

    Update LLCMod.cs

--- src/LLCMod.cs
+++ src/LLCMod.cs
@@ -104,48 +104,48 @@ public class LLCMod : BasePlugin
         try
         {
             //Check BepInEx mod
-            if (IL2CPPChainloader.Instance.Plugins.Keys.Any(guid => !guid.StartsWith("Com.Bright.")))
-                return OpenGlobalPopup("Invalid assemblies.\nUnable to run game.");
+//            if (IL2CPPChainloader.Instance.Plugins.Keys.Any(guid => !guid.StartsWith("Com.Bright.")))
+//                return OpenGlobalPopup("Invalid assemblies.\nUnable to run game.");
             //Check visual mod
-            if (OperatingSystem.IsWindows())
+//            if (OperatingSystem.IsWindows())
             {
-                var steamPath =
-                    Registry.GetValue(@"HKEY_CURRENT_USER\Software\Valve\Steam", "SteamPath", null) as string;
-                var userdata = long.Parse(LoginInfoManager.Instance.SteamID) - 76561197960265728;
-                var lines = File.ReadAllLines($"{steamPath}/userdata/{userdata}/config/localconfig.vdf");
-                var start = false;
-                var end = false;
-                var has = false;
-                foreach (var line in lines)
+ //               var steamPath =
+ //                   Registry.GetValue(@"HKEY_CURRENT_USER\Software\Valve\Steam", "SteamPath", null) as string;
+ //               var userdata = long.Parse(LoginInfoManager.Instance.SteamID) - 76561197960265728;
+ //               var lines = File.ReadAllLines($"{steamPath}/userdata/{userdata}/config/localconfig.vdf");
+ //               var start = false;
+ //               var end = false;
+ //               var has = false;
+ //               foreach (var line in lines)
                 {
-                    if (!start)
+ //                   if (!start)
                     {
-                        if ("\t\t\t\t\t\"1973530\"".Equals(line))
-                            start = true;
-                        continue;
-                    }
+ //                       if ("\t\t\t\t\t\"1973530\"".Equals(line))
+ //                           start = true;
+  //                      continue;
+                   }
 
-                    if (line.Contains("\"LastPlayed\""))
-                        if (!end)
-                            end = true;
-                        else
-                            break;
-                    var launchOptions = "\t\t\t\t\t\t\"LaunchOptions\"\t\t";
-                    if (!line.Contains(launchOptions)) continue;
-                    launchOptions = line[launchOptions.Length..];
-                    if (launchOptions.Contains("%command%"))
-                        has = true;
-                    break;
+  //                  if (line.Contains("\"LastPlayed\""))
+  //                      if (!end)
+  //                          end = true;
+  //                      else
+  //                          break;
+  //                  var launchOptions = "\t\t\t\t\t\t\"LaunchOptions\"\t\t";
+  //                  if (!line.Contains(launchOptions)) continue;
+  //                  launchOptions = line[launchOptions.Length..];
+  //                  if (launchOptions.Contains("%command%"))
+  //                      has = true;
+   //                 break;
                 }
 
-                if (has) return OpenGlobalPopup("Invalid parameters.\nUnable to run game.");
+    //            if (has) return OpenGlobalPopup("Invalid parameters.\nUnable to run game.");
             }
-
-            //Check private server
-            var hostEntry = Dns.GetHostEntry("www.limbuscompanyapi.com");
-            var ip = hostEntry.AddressList[0].ToString();
-            if (ip.StartsWith("127.0.") || ip.StartsWith("192.168."))
-                return OpenGlobalPopup("Remote address loopback error.\nUnable to run game.");
+//
+   //         //Check private server
+   //         var hostEntry = Dns.GetHostEntry("www.limbuscompanyapi.com");
+   //         var ip = hostEntry.AddressList[0].ToString();
+   //         if (ip.StartsWith("127.0.") || ip.StartsWith("192.168."))
+   //             return OpenGlobalPopup("Remote address loopback error.\nUnable to run game.");
         }
         catch (Exception e)
         {
